1378c05741a613db0152cb76da9170ff43b62b4f
Gosuke Miyashita <gosukenator@gmail.com>
2012-01-31 16:09:58 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gorgan::Application.config.secret_token = '7a882b348c0fc0d7f2880c1c3c66210495cb1bd6b60576d8c6ad3270a1c4ca82622b4bd75e0e57a96969a9f47c42bf74a611a980bd8111821a5deb785c6e956e'

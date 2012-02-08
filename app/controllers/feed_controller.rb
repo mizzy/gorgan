254397e6f4a576cbea9cc957e2b475f91a37c0f7
Gosuke Miyashita <gosukenator@gmail.com>
2012-02-08 23:06:15 +0900

    commit

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,27 +1,19 @@
 class FeedController < ApplicationController
+  include ApplicationHelper
+
   def index
     if not session[:access_token]
       redirect_to '/auth/github'
       return
     end
-
-    @orgs = client.orgs
   end
 
   def org
-    @org   = client.org(params[:org])
-    @repos = client.org_repos(params[:org])
+    @org   = octokit.org(params[:org])
+    @repos = octokit.org_repos(params[:org])
     @repos.each do |repo|
-      repo["commits"] = client.commits("#{params[:org]}/#{repo['name']}")
+      repo["commits"] = octokit.commits("#{params[:org]}/#{repo['name']}")
     end
-  end
-
-  private
-  def client
-    Octokit::Client.new(
-        login: 'me',
-        oauth_token: session[:access_token],
-    )
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def octokit
+    Octokit::Client.new(
+        login: 'me',
+        oauth_token: session[:access_token],
+    )
+  end
+
 end
